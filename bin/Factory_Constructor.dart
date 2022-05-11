369d11d584a911aa3d455cb88d97706c6e0e1b98
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-11 22:39:05 +0700

    lean factory constructor

---
+++ bin/Factory_Constructor.dart
@@ -0,0 +1,22 @@
+class Database {
+
+  Database() {
+    print('Create new database connection');
+  }
+
+  // This made variable global
+  static Database database = Database();
+
+  factory Database.get() {
+    return database;
+  }
+
+}
+
+void main() {
+  // This will get object
+  var database1 = Database.get();
+  var database2 = Database.get();
+
+  print(database1 == database2);
+}
