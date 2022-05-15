05e81d3b91daad3a881eaf05dd9ee8cf538dfd7b
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-15 23:18:28 +0700

    learn getHashcode

--- bin/data/category.dart
+++ bin/data/category.dart
@@ -4,18 +4,38 @@ class Category {
 
   Category(this.id, this.name);
 
-  // This will be override equals method
-  bool operator ==(Object other) {
-    if(other is Category) {
-      if(id != other.id) {
-        return false;
-      } else if((name != other.name)) {
-        return false;
-      } else {
-        return true;
+  // use Generator
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Category &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          name == other.name;
+
+  @override
+  int get hashCode => id.hashCode ^ name.hashCode;
+
+  // Manualy
+//This will be override equals method
+      bool operator ==(Object other) {
+        if(other is Category) {
+          if(id != other.id) {
+            return false;
+          } else if((name != other.name)) {
+            return false;
+          } else {
+            return true;
+          }
+        } else {
+          return false;
+        }
       }
-    } else {
-      return false;
-    }
-  }
+
+      // This to added hasCode getter
+      int get hashCode {
+          var result = id.hashCode;
+          result += name.hashCode;
+          return result;
+      }
 }
