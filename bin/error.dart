727970ca0c66836becf0a874172b10b3195f7f01
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-16 22:23:29 +0700

    learn how to handle error

---
+++ bin/error.dart
@@ -0,0 +1,10 @@
+void main() {
+
+  var list = ["Stefanus", "Frans", "Sebastian"];
+
+  if(list.length > 10) {
+    print("List is not too long");
+  } else {
+    print("List is too long");
+  }
+}
